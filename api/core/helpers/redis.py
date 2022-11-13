161be643b89be1ce50a0ea6fce671f92cb9a3e5b
Matthew Wisdom <matthewwisdom11@gmail.com>
2022-11-13 17:05:20 +0100

    Remove username/password

--- api/core/helpers/redis.py
+++ api/core/helpers/redis.py
@@ -4,7 +4,5 @@ import aioredis
 from core.config import config
 
 redis = aioredis.from_url(
-    url=f"redis://{config.REDIS_HOST}:{config.REDIS_PORT}",
-    username=os.getenv("REDIS_USER"),
-    password=os.getenv("REDIS_PASS"),
+    url=f"redis://{config.REDIS_HOST}:{config.REDIS_PORT}"
 )
