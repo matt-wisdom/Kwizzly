e195790b85015ec47cfb858556a2f4ee4b1ab9f2
Matthew Wisdom <matthewwisdom11@gmail.com>
2022-11-13 17:04:57 +0100

    Add auth to mongodb config

--- api/core/config.py
+++ api/core/config.py
@@ -7,19 +7,16 @@ class Config(BaseSettings):
     ENV: str = "development"
     DEBUG: bool = True
     APP_HOST: str = "0.0.0.0"
-    APP_PORT: int = 8000
+    APP_PORT: int = 8080
     DEFAULT_TOKEN_EXPIRES: int = 15 * 86400  # 15 days
-    DB_URL: str = "mysql+aiomysql://fastapi:fastapi@localhost:3306/fastapi"
     JWT_SECRET_KEY: str = "fastapi"
     JWT_ALGORITHM: str = "HS256"
     SENTRY_SDN: str = None
-    CELERY_BROKER_URL: str = "amqp://user:bitnami@localhost:5672/"
-    CELERY_BACKEND_URL: str = "redis://redis:6379/0"
     REDIS_HOST: str = (
-        "localhost"  # "redis-11660.c258.us-east-1-4.ec2.cloud.redislabs.com"
+        "redis"
     )
     REDIS_PORT: int = 6379
-    MONGO_DATABASE_URI = "mongodb://db:27017"
+    MONGO_DATABASE_URI = "mongodb://root:example@db:27017"
     MONGO_APP_DATABASE = "test_all" if os.getenv("TESTING") else "test_app"
     MONGO_USER_COLLECTION = "test_users"
     MONGO_TEXTS_COLLECTION = "test_texts"
@@ -42,24 +39,12 @@ class DevelopmentConfig(Config):
     REDIS_PORT: int = 6379
 
 
-#
-
-
 class LocalConfig(Config):
-    DB_URL: str = (
-        ""
-        if os.getenv("TESTING")
-        else "mysql+aiomysql://fastapi:fastapi@localhost:3306/fastapi"
-    )
+    pass
 
 
 class ProductionConfig(Config):
     DEBUG: str = False
-    DB_URL: str = (
-        ""
-        if os.getenv("TESTING")
-        else "mysql+aiomysql://fastapi:fastapi@localhost:3306/prod"
-    )
 
 
 def get_config():
